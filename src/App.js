60c4885936652ae0ddaf79b0713889e971911a08
fokichevaM <juzzii7@gmail.com>
2023-02-09 21:43:05 +0200

    - remmove readme - add login route

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </Router>
   );
